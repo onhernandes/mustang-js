1eab1cc6b3365d621deaa5e2955b8086bb118315
devmath <midia.matheus@gmail.com>
2016-08-11 23:45:35 -0300

    set files

--- docs/mustang.js
+++ docs/mustang.js
@@ -1,83 +1,110 @@
-$(document).ready(function() {
-	var Mustang = function(wrapper) {
-		var wrapper = $(wrapper),
-			slide = $(".banner"),
-			total = slide.length,
-			translate = 0,
-			time = 3000,
-			counter = 0;
+/*
+ *  Project: 
+ *  Description: 
+ *  Author: 
+ *  License: 
+ */
 
-		function move() {
-			translate += 100;
-			counter++;
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ( $, window, document, undefined ) {
 
-			if (counter < total) {
-				var final = "translateX(-" + translate + "%)";
-			} else {
-				var final = "translateX(0%)";
-				counter = 0;
-				translate = 0;
-			}
+    // undefined is used here as the undefined global variable in ECMAScript 3 is
+    // mutable (ie. it can be changed by someone else). undefined isn't really being
+    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+    // can no longer be modified.
 
-			slide.css("transform", final);
-		}
-		
-		var interval = setInterval(move, time);
-	}
+    // window is passed through as local variable rather than global
+    // as this (slightly) quickens the resolution process and can be more efficiently
+    // minified (especially when both are regularly referenced in your plugin).
 
-	Mustang(".slide", ".banner");
-});
+    // Create the defaults once
+    var pluginName = 'defaultPluginName',
+        defaults = {
+            propertyName: "value"
+        };
 
-;(function($,) {
-	"use strict";
+    // The actual plugin constructor
+    function Plugin( element, options ) {
+        this.element = element;
 
-		
-	var plugin = "Mustang";
+        // jQuery has an extend method which merges the contents of two or
+        // more objects, storing the result in the first object. The first object
+        // is generally empty as we don't want to alter the default options for
+        // future instances of the plugin
+        this.options = $.extend( {}, defaults, options) ;
 
-	// The actual plugin constructor
-	function Plugin (element, options) {
-		this.element = element;
-		this.options = {
-			translate: 0,
-			time = 3000,
-			final = "",
-			counter = 0
-		};
+        this._defaults = defaults;
+        this._name = pluginName;
 
-		this.init();
-	}
+        this.init();
+    }
 
-	// Avoid Plugin.prototype conflicts
-	$.extend( Plugin.prototype, {
-		init: function() {
-			$.extend(this.options, options);
-            setInterval(this.move, this.time);
-		},
+    Plugin.prototype.init = function () {
+        // Place initialization logic here
+        // You already have access to the DOM element and the options via the instance,
+        // e.g., this.element and this.options
+    };
 
-		move: function() {
-			this.options.translate += 100;
-			this.options.counter++;
+    // You don't need to change something below:
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations and allowing any
+    // public function (ie. a function whose name doesn't start
+    // with an underscore) to be called via the jQuery plugin,
+    // e.g. $(element).defaultPluginName('functionName', arg1, arg2)
+    $.fn[pluginName] = function ( options ) {
+        var args = arguments;
 
-			if (this.options.counter < this.options.total) {
-				this.options.final = "translateX(-" + this.options.translate + "%)";
-			} else {
-				this.options.final = "translateX(0%)";
-				this.options.counter = 0;
-				this.options.translate = 0;
-			}
+        // Is the first parameter an object (options), or was omitted,
+        // instantiate a new instance of the plugin.
+        if (options === undefined || typeof options === 'object') {
+            return this.each(function () {
 
-			this.element.css("transform", this.options.final);
-		}
-	} );
+                // Only allow the plugin to be instantiated once,
+                // so we check that the element has no plugin instantiation yet
+                if (!$.data(this, 'plugin_' + pluginName)) {
 
-	// A really lightweight plugin wrapper around the constructor,
-	// preventing against multiple instantiations
-	$.fn[ plugin ] = function( options ) {
-		return this.each( function() {
-			if ( !$.data( this, "plugin_" + plugin ) ) {
-				$.data( this, "plugin_" +
-					plugin, new Plugin( this, options ) );
-			}
-		} );
-	};
-})(jQuery);
+                    // if it has no instance, create a new one,
+                    // pass options to our plugin constructor,
+                    // and store the plugin instance
+                    // in the elements jQuery data object.
+                    $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+                }
+            });
+
+        // If the first parameter is a string and it doesn't start
+        // with an underscore or "contains" the `init`-function,
+        // treat this as a call to a public method.
+        } else if (typeof options === 'string' && options[0] !== '_' && options !== 'init') {
+
+            // Cache the method call
+            // to make it possible
+            // to return a value
+            var returns;
+
+            this.each(function () {
+                var instance = $.data(this, 'plugin_' + pluginName);
+
+                // Tests that there's already a plugin-instance
+                // and checks that the requested public method exists
+                if (instance instanceof Plugin && typeof instance[options] === 'function') {
+
+                    // Call the method of our plugin instance,
+                    // and pass it the supplied arguments.
+                    returns = instance[options].apply( instance, Array.prototype.slice.call( args, 1 ) );
+                }
+
+                // Allow instances to be destroyed via the 'destroy' method
+                if (options === 'destroy') {
+                  $.data(this, 'plugin_' + pluginName, null);
+                }
+            });
+
+            // If the earlier cached method
+            // gives a value back return the value,
+            // otherwise return this to preserve chainability.
+            return returns !== undefined ? returns : this;
+        }
+    };
+
+}(jQuery, window, document));
---
+++ docs/old.mustang.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	/*var Mustang = function(wrapper) {
+		var wrapper = $(wrapper),
+			slide = $(".banner"),
+			total = slide.length,
+			translate = 0,
+			time = 3000,
+			counter = 0;
+
+		function move() {
+			translate += 100;
+			counter++;
+
+			if (counter < total) {
+				var final = "translateX(-" + translate + "%)";
+			} else {
+				var final = "translateX(0%)";
+				counter = 0;
+				translate = 0;
+			}
+
+			slide.css("transform", final);
+		}
+		
+		var interval = setInterval(move, time);
+	}
+
+	Mustang(".slide", ".banner");
+
+	Mustang(".slide", {
+		translate: 0,
+		time: 3000,
+		final: "3000",
+	});*/
+
+
+});
